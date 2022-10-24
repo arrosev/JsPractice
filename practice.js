1a974786c33c25be3b45f7116da4f7a169b05086
Arrose <arrose.chen@foxmail.com>
2022-10-24 20:50:45 +0800

    stack && queue

---
+++ practice.js
@@ -0,0 +1,16 @@
+
+// import Stack from "./stack.js";
+
+// const stack = new Stack()
+
+// stack.push(1)
+// stack.push(2)
+// stack.push(4)
+// stack.push(2)
+// stack.push(3)
+
+// console.log(stack.peek())
+// stack.pop()
+// console.log(stack.peek())
+// console.log(stack.toString())
+
